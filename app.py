3931cf207cec029673076a0eb52a4fc19da89c09
Vitor Hugo de Souza <39071124+VitorHugoSouza@users.noreply.github.com>
2023-03-23 19:55:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ template_dir = os.path.join(WEBAPP_ROOT, "template")
 app = Flask(__name__, template_folder=template_dir) 
 model = load('melhor_modelo.joblib')
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+@app.route("/predict",methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
